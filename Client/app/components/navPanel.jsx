c39aa832857f40c9e8e507519bbe6312fa5099fb
IFeaskoI <ivanakovenko06758@gmail.com>
2023-10-20 20:58:06 +0300

    h-[120px]

--- Client/app/components/navPanel.jsx
+++ Client/app/components/navPanel.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function NavPanel() {
     return (
-        <nav className="flex flex-row justify-center items-center w-full h-[70px] bg-slate-300">
+        <nav className="flex flex-row justify-center items-center w-full h-[120px] bg-slate-300">
             <div className='flex flex-row items-center space-x-[20px] w-auto h-auto'>
                 <Link href='/'>
                     <button className='bg-purple-700 rounded-md p-3'>Main Page</button>
