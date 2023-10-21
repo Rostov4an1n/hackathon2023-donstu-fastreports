a29443ea602b3ae4ccfdfb64fed8e98abd4e367f
RainGo161 <RainGo161@bk.ru>
2023-10-21 14:36:32 +0300

    ДобавленBackground графика а такж  убран график бар

--- Client/app/components/chartLine.js
+++ Client/app/components/chartLine.js
@@ -1,5 +1,3 @@
-'use client';
-
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -8,67 +6,68 @@ import {
     LineElement,
     Title,
     Tooltip,
-    Legend,
-  } from 'chart.js';
-  
-  ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
     Legend
-  );
-  
-  export const optionsLine = {
+} from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export const optionsLine = {
     responsive: true,
     interaction: {
-      mode: 'index',
-      intersect: false,
+        mode: 'index',
+        intersect: false,
     },
     stacked: false,
     plugins: {
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart - Multi Axis',
-      },
+        title: {
+            display: false,
+            text: 'Chart.js Line Chart - Multi Axis',
+        },
     },
     scales: {
-      y: {
-        type: 'linear',
-        display: true,
-        position: 'left',
-      },
-      y1: {
-        type: 'linear',
-        display: true,
-        position: 'right',
-        grid: {
-          drawOnChartArea: false,
+        y: {
+            type: 'linear',
+            display: true,
+            position: 'none',
         },
-      },
+        y1: {
+            type: 'linear',
+            display: true,
+            position: 'none',
+            grid: {
+                drawOnChartArea: false,
+            },
+        },
     },
-  };
-  
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  
-  export const dataLine = {
+    elements: {
+        line: {
+            fill: true,
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            tension: 0.4,
+        },
+    },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+const datalabels = [20, 18, 16, 13, 10, 9, 12, 3, 1];
+const datalabels2 = [23, 21, 19, 16, 13, 12, 15, 6, 4];
+
+export const dataLine = {
     labels,
-    datasets: [
-      {
-        label: 'Dataset 1',
-        data: labels.map(() => Math.floor(Math.random() * 1000)),
-        borderColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-        yAxisID: 'y',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => Math.floor(Math.random() * 1000)),
-        borderColor: 'rgb(53, 162, 235)',
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
-        yAxisID: 'y1',
-      },
+    datasets: [{
+            label: 'Dataset 1',
+            data: datalabels,
+            borderColor: '#40BE53FF',
+            backgroundColor: 'rgba(64, 190, 83, 0.5)',
+            yAxisID: 'y',
+        },
+        {
+            label: 'Dataset 2',
+            data: datalabels2,
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            yAxisID: 'y1',
+        },
     ],
-  };
+};
