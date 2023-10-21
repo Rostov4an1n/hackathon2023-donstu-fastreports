3202b74a613ec4034637ede675938cd5ed5431e6
RainGo161 <RainGo161@bk.ru>
2023-10-21 17:10:24 +0300

    Внесена кастомизация диаграммы

--- Client/app/components/chartLine.js
+++ Client/app/components/chartLine.js
@@ -1,73 +1,93 @@
 import {
-    Chart as ChartJS,
+    Chart as ChartJS, // Импортируем необходимые компоненты и модули из библиотеки Chart.js
     CategoryScale,
     LinearScale,
     PointElement,
     LineElement,
     Title,
     Tooltip,
-    Legend
+    Filler
 } from 'chart.js';
 
-ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Filler);
+// Регистрируем необходимые компоненты в Chart.js
 
-export const optionsLine = {
-    responsive: true,
+export const optionsLine = { // Настройки для графика Line Chart
+    responsive: true, // График будет адаптивным к размеру контейнера
     interaction: {
-        mode: 'index',
-        intersect: false,
+        mode: 'index', // Режим взаимодействия при наведении
+        intersect: false, // Необходимо, чтобы подсветка не пересекалась
     },
-    stacked: false,
+    stacked: false, // График не будет складываться (не применимо к Line Chart)
     plugins: {
-        title: {
-            display: false,
-            text: 'Chart.js Line Chart - Multi Axis',
+        legend: { // отображение маркеров
+            display: false, // Не отображать заголовок
+            text: 'Граффик доходов/расходов', // Заголовок графика
         },
     },
-    scales: {
+    scales: { // Настройки осей
         y: {
-            type: 'linear',
+            type: 'linear', // Тип оси - линейная
+            display: true, // Отображать ось
+            position: 'none', // Не показывать значения оси
+            grid: {
+                borderDash: [3, 3],
+            }
+        },
+        x: {
+            type: 'category', // Предполагая, что это категориальная ось x
             display: true,
-            position: 'none',
+            grid: {
+                drawOnChartArea: false, // Не рисовать сетку на графике
+            },
         },
         y1: {
-            type: 'linear',
-            display: true,
-            position: 'none',
+            type: 'linear', // Тип оси - линейная
+            display: true, // Отображать ось
+            position: 'none', // Не показывать значения оси
             grid: {
-                drawOnChartArea: false,
+                drawOnChartArea: false, // Не рисовать сетку на графике
             },
         },
     },
     elements: {
         line: {
-            fill: true,
-            backgroundColor: 'rgba(255, 0, 0, 0.5)',
-            tension: 0.4,
+            tension: 0.5, // Натяжение линии графика (плавность)
         },
     },
 };
 
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']; // Метки для оси x
 
-const datalabels = [20, 18, 16, 13, 10, 9, 12, 3, 1];
-const datalabels2 = [23, 21, 19, 16, 13, 12, 15, 6, 4];
+const datalabels = [20, 18, 16, 13, 10, 9, 12, 3, 1, 20, 18, 16]; // Данные для Dataset 1
+const datalabels2 = [29, 27, 25, 22, 19, 18, 21, 22, 1, 29, 27, 25]; // Данные для Dataset 2
 
 export const dataLine = {
-    labels,
+    labels, // Подставляем метки для оси x
     datasets: [{
-            label: 'Dataset 1',
-            data: datalabels,
-            borderColor: '#40BE53FF',
-            backgroundColor: 'rgba(64, 190, 83, 0.5)',
-            yAxisID: 'y',
+            label: false, // Название Dataset 1
+            data: datalabels, // Данные для Dataset 1
+            backgroundColor: '#FF6666FF', // Цвет заливки графика (красный)
+            borderColor: "#FF6666FF", // Цвет границы графика (красный)
+            yAxisID: 'y', // Использовать ось y
+            fill: {
+                target: 'origin', // Заполнить область под графиком
+                above: '#FF666644', // Цвет заливки выше графика (красный с прозрачностью)
+                below: '#F9000000', // Цвет заливки ниже графика (красный с прозрачностью)
+            },
+            borderDashOffset: 3,
         },
         {
-            label: 'Dataset 2',
-            data: datalabels2,
-            borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)',
-            yAxisID: 'y1',
+            label: false, // Название Dataset 2
+            data: datalabels2, // Данные для Dataset 2
+            backgroundColor: '#42BD53FF', // Цвет заливки графика (зеленый)
+            borderColor: "#42BD53FF", // Цвет границы графика (зеленый)
+            yAxisID: 'y1', // Использовать ось y1
+            fill: {
+                target: 'origin', // Заполнить область под графиком
+                above: '#42BD533D', // Цвет заливки выше графика (зеленый с прозрачностью)
+                below: '#F9000000' // Цвет заливки ниже графика (красный с прозрачностью)
+            }
         },
     ],
-};
+}
--- Client/app/page.jsx
+++ Client/app/page.jsx
@@ -1,18 +1,16 @@
 'use client';
-
-import React from 'react'; // Make sure to import React
+import React from 'react';
 import { Bar, Line } from 'react-chartjs-2';
 import { optionsBar, dataBar } from './components/chartSales';
 import { optionsLine, dataLine } from './components/chartLine';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className='w-[588px] h-[304px] flex justify-center items-center'>
-        <Bar options={optionsBar} data={dataBar} />
-      </div>
-      <div className='w-[588px] h-[304px] flex justify-center items-center'>
-        <Line options={optionsLine} data={dataLine} />
+    <main className="h-screen w-screen flex justify-center bg-[#EEF1F8FF]">
+      <div className='w-[80%] h-screen mx-26 my-16 '>
+        <div className='w-1/3 h-[200px] flex justify-center items-center'>
+          <Line options={optionsLine} data={dataLine}/>
+        </div>
       </div>
     </main>
   );
