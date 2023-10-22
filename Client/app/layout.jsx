3a0f35af90a90b5ca819955f58445fea5c35bfbe
Rostov4anin <mmaksguy@gmail.com>
2023-10-22 09:59:34 +0300

    rea

--- Client/app/layout.jsx
+++ Client/app/layout.jsx
@@ -1,21 +1,22 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import NavPanel from './components/navPanel'
+import { ReactQueryProvider } from './reactQueruProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Tinkoff App',
-  description: 'Hackathon app for Tinkoff',
+  title: 'Fast Reports',
+  description: 'Hackathon app for Fast Reports',
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <NavPanel />
-        {children}
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+        </body>
+      </html>
+    </ReactQueryProvider>
   )
 }
