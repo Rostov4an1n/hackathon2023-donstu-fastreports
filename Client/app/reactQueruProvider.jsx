d3c055e1c784e6dbb2b2f51bab15807d729ac647
Rostov4anin <mmaksguy@gmail.com>
2023-10-22 09:54:40 +0300

    test query

---
+++ Client/app/reactQueruProvider.jsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({ children }) => {
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+};
